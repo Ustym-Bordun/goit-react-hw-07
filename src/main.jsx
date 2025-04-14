d09ee62f3dff1c4e26563619b6cc0ef8f67f2c9a
Ustym Bordun <enderqwer12@gmail.com>
2025-04-15 00:29:14 +0300

    add strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import './index.css';
 import App from './App/App';
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  // </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
